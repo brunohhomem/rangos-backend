b0d9c723ffce3a571ffdaab4624d94b8822b7fab
Bruno H. Homem <brunohhomem@gmail.com>
2025-02-10 06:02:35 -0300

    feat(user): implement `findByEmail`

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -9,6 +9,11 @@ export class UserController {
   @Post('/create-user')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Get(':email')
+  findOne(@Param('email') email: string) {
+    return this.userService.findByEmail(email);
   }
 
   // @Get()
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -18,9 +18,15 @@ export class UserService {
     return { ...createdUser, password: undefined };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
   }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
 
   // findAll() {
   //   return `This action returns all user`;
