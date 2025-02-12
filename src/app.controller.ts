bce925edd1dccae6df0292bcf12781dc162114e6
Bruno H. Homem <brunohhomem@gmail.com>
2025-02-12 05:16:17 -0300

    feat(auth): implement `@CurrentUser`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './auth/decorators/is-public.decorator';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('/api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get('health')
   async getHealthCheck() {
     return this.appService.getHealthCheck();
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User) {
+    return user;
   }
 }
