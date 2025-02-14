488da5266ff3130192a0dbb59aa932521abbf149
Bruno H. Homem <brunohhomem@gmail.com>
2025-02-14 05:17:34 -0300

    feat(foods): fix findByFood and implement delete

--- src/foods/foods.controller.ts
+++ src/foods/foods.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FoodsService } from './foods.service';
 import { CreateFoodDto } from './dto/create-food.dto';
+import { Food } from './entities/food.entity';
 
 @Controller('/api/v1/foods')
 export class FoodsController {
@@ -19,28 +28,36 @@ export class FoodsController {
   @Get('food')
   async getFoods(
     @Query('search') search: string,
-    @Query('page') page: number = 1,
-    @Query('pageSize') pageSize: number = 10,
+    @Query('page') page: string = '1',
+    @Query('pageSize') pageSize: string = '10',
+    @Query('orderBy') orderBy: keyof Food = 'food',
+    @Query('order') order: 'asc' | 'desc' = 'asc',
+    @Query('isVeg') isVeg?: string,
+    @Query('isVegan') isVegan?: string,
   ) {
     return await this.foodsService.findByFood(
       search || '',
       Number(page),
       Number(pageSize),
+      orderBy,
+      order,
+      isVeg ? isVeg === 'true' : undefined,
+      isVegan ? isVegan === 'true' : undefined,
     );
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.foodsService.findOne(+id);
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.foodsService.findById(+id);
+  }
 
   // @Patch(':id')
   // update(@Param('id') id: string, @Body() updateFoodDto: UpdateFoodDto) {
   //   return this.foodsService.update(+id, updateFoodDto);
   // }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.foodsService.remove(+id);
-  // }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.foodsService.remove(+id);
+  }
 }
--- src/foods/foods.service.ts
+++ src/foods/foods.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFoodDto } from './dto/create-food.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Food } from './entities/food.entity';
 
 @Injectable()
 export class FoodsService {
@@ -24,21 +25,38 @@ export class FoodsService {
     });
   }
 
-  findByFood(food: string, page: number = 1, pageSize: number = 10) {
+  findByFood(
+    food: string,
+    page: number = 1,
+    pageSize: number = 10,
+    orderBy: keyof Food = 'food',
+    order: 'asc' | 'desc' = 'asc',
+    isVeg?: boolean,
+    isVegan?: boolean,
+  ) {
     return this.prisma.food.findMany({
       where: {
         food: { contains: food, mode: 'insensitive' },
+        isActive: true, // Apenas alimentos ativos
+        ...(isVeg !== undefined && { isVeg }),
+        ...(isVegan !== undefined && { isVegan }),
       },
+      orderBy: { [orderBy]: order },
       skip: (page - 1) * pageSize,
       take: pageSize,
     });
   }
 
-  // update(id: number, updateFoodDto: UpdateFoodDto) {
-  //   return `This action updates a #${id} food`;
+  // update(id: number) {
+  //   const food = this.prisma.food.update({
+  //     where: {id},
+  //   })
+
   // }
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} food`;
-  // }
+  remove(id: number) {
+    this.prisma.food.delete({
+      where: { id },
+    });
+  }
 }
