4708af0d45be929685408d751cd5157a3a8e8e01
Bruno H. Homem <brunohhomem@gmail.com>
2025-02-18 06:17:47 -0300

    feat(app):  getHealthCheck test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -5,15 +5,17 @@ import { AppService } from './app.service';
 describe('AppController', () => {
   let controller: AppController;
 
-  beforeEach(async () => {
+  const mockAppService = {
+    getHealthCheck: jest.fn(),
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [
         {
           provide: AppService,
-          useValue: {
-            getHello: jest.fn(() => 'Hello World'),
-          },
+          useValue: mockAppService,
         },
       ],
     }).compile();
@@ -21,7 +23,19 @@ describe('AppController', () => {
     controller = module.get<AppController>(AppController);
   });
 
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('getHealthCheck', async () => {
+    await controller.getHealthCheck();
+
+    expect(mockAppService.getHealthCheck).toHaveBeenCalledTimes(1);
+    expect(mockAppService.getHealthCheck).toHaveBeenCalledWith();
+    expect(mockAppService.getHealthCheck).toHaveReturned();
   });
 });
